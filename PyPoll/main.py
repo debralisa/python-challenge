a2627a8425b096efc9a3d0c09002823dabdff780
debralisa <123344011+debralisa@users.noreply.github.com>
2023-03-12 14:28:51 -0400

    Cleaning up comments, variables, etc

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,29 @@
 #python challenge PyPoll script
-<<<<<<< HEAD
+
 #Dependencies
 import os
 import csv
 
 #from pathlib import Path
-#election_data_csv = Path("python-challenge", "PyPoll", "election_data.csv")
-election_data_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'election_data.csv')
-election_out_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'analysis', 'election_data_out.txt')
+#election_data_csv = Path("python-challenge", "PyPoll", "election_data.csv") 
 
 #File locations for read and write files
 #election_data_csv = "/Users/debralisa3/Dropbox/python-challenge/PyPoll/Resources/election_data.csv"
 #election_out_txt = "/Users/debralisa3/Dropbox/python-challenge/PyPoll/Analysis/election_data_out.txt"
 
-#Total vote counter
+#path locations for input and output files. First couple unsuccessful attempts are above and commented out to show progression. 
+
+election_data_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'election_data.csv')
+election_out_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'analysis', 'election_data_out.txt')
+
+#Set total vote counter
 vote_total = 0
 
-#Candidate options and vote counters
+#Setup candidate options and vote counters
 candidate_options = []
 candidate_votes = {}
 
-#Winning candidate and winning count trackers
+#Setup winning candidate variable and winning counter
 winning_candidate = ""
 winning_count = 0
 
@@ -28,29 +31,27 @@ winning_count = 0
 with open(election_data_csv, "r") as election_data:
     reader = csv.DictReader(election_data)
 
-    #For each row
+    #For statement to run through each row
     for row in reader:
 
-        #add to the total vote count and get candidate name per row
+        #add/increment vote count to the total vote count and get candidate name per row
         vote_total = vote_total + 1
         candidate_name = row["Candidate"]
 
-        #if condition for no candidate match
+        #if condition for no candidate name match
         if candidate_name not in candidate_options:
 
-            #append name to the candidate names list and
-            #start candidate vote count
+            #append name to the candidate names list and start specific candidate vote counter
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
     
-        #then add vote to specific candidate count
+        #then add/increment vote count to specific candidate vote counter
         candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
 
-#Print results and export data to text file
+#Print results to screen and write data to output text file
 with open(election_out_txt, "w") as txt_file:
 
-    #Print final results
-    election_results = (
+     election_results = (
         f"\nElection Results\n"
         f"______________________\n"
         f"Total Votes:  {vote_total}\n"
@@ -58,10 +59,9 @@ with open(election_out_txt, "w") as txt_file:
     )
     print(election_results)
 
-    #save final vote count to file
     txt_file.write(election_results)
 
-    #Calculate the winner with for loop out counts
+    #Next module is to calculate the candidates' percentages and vote counts with a for loop for each candidate
     for candidate in candidate_votes:
         votes = candidate_votes.get(candidate)
         vote_percentage = float(votes)/float(vote_total) * 100
@@ -71,11 +71,11 @@ with open(election_out_txt, "w") as txt_file:
             winning_count = votes
             winning_candidate = candidate
 
-        #Print each candidate and voter count
+        #Print each candidate, percentage (to the 3rd decimal) and actual voter count
         voter_output = f"{candidate}: {vote_percentage: .3f}% ({votes})\n"
         print(voter_output)
 
-        #Save the candidate voter counts and perc to text file
+        #Save the candidate voter summary information from the print calculations to the output text file
         txt_file.write(voter_output)
 
 
@@ -88,6 +88,6 @@ with open(election_out_txt, "w") as txt_file:
 
     print (winning_candidate_summary)
 
-    #save winner to text file
+    #save winner information to the output text file
     txt_file.write(winning_candidate_summary)
 
